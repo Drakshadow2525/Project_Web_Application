19d0405f2cbfde6459e0e346849c912bfb8ba277
Drakshadow2525 <63239062+Drakshadow2525@users.noreply.github.com>
2020-04-06 20:33:21 +0700

    Add files via upload

---
+++ Chat.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Chat.aspx.cs" Inherits="FinalProjectPetey.Chat" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        My chat<br />
+        <br />
+        <asp:ListBox ID="ListBox1" runat="server" Height="259px" Width="718px"></asp:ListBox>
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server" Width="368px"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </p>
+        <p>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Chat.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalProjectPetey
+{
+    public partial class Chat : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<string> saveChat;
+            if (Application["chat"] != null) {
+                saveChat = (List<string>)Application["chat"];
+            foreach (string x in (List<string>) Application["chat"])
+            {
+                    ListBox1.Items.Add(x);
+            } 
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            List<string> saveChat;
+            if(Application["chat"] != null)
+            {
+                saveChat = (List<string>)Application["chat"];
+            }
+            else
+            {
+                saveChat = new List<string>();
+            }
+            saveChat.Add(TextBox2.Text + "  :  " + TextBox1.Text);
+            Application["chat"] = saveChat;
+            Response.RedirectPermanent("chat.aspx");
+        }
+    }
+}
